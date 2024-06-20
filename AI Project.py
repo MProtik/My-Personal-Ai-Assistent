c3aba091b68b2e669a2410b216719a328b0e67c6
MProtik <149885997+MProtik@users.noreply.github.com>
2024-06-20 20:33:18 +0600

    Update AI Project.py
    some changes are made due to voice not working in mac

--- AI Project.py
+++ AI Project.py
@@ -4,10 +4,10 @@ import datetime
 import requests
 import webbrowser
 
-engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[14].id) #14 is good, 38 is usable
 
 rate = engine.getProperty('rate')
 engine.setProperty('rate', 150)
@@ -45,55 +45,55 @@ def wish():
         speak('Good Afternoon')
     else:
         speak('Good Evening')
-    get_weather("Narsingdi")
+    #get_weather("Narsingdi")
     speak('THIS IS JARVIS')
     speak("At your service. Tell me how can i help you")
     
 
 
-def get_weather(city):
-    api = 'aa25dfa89b8526f5bdeffc46c6492a32'
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
-    response = requests.get(url)
-    data = response.json()
+# def get_weather(city):
+#     api = 'aa25dfa89b8526f5bdeffc46c6492a32'
+#     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
+#     response = requests.get(url)
+#     data = response.json()
 
-    weather_descriptions = {
-    "Clear Conditions": ["Clear sky"],
-    "Cloudy Conditions": ["Few clouds", "Scattered clouds", "Broken clouds", "Overcast clouds"],
-    "Atmospheric Conditions": ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand, dust whirls", 
-                               "Dust, low visibility", "Sand"],
-    "Poor Visibility Conditions": ["Haze", "Dust, low visibility"],
-    "Windy Conditions": ["Squalls", "Tornado"],
-    "Rain Conditions": ["Light rain showers", "Moderate rain showers", "Heavy rain showers",
-                        "Light rain", "Moderate rain", "Heavy rain", "Thunderstorm with light rain",
-                        "Thunderstorm with moderate rain", "Thunderstorm with heavy rain",
-                        "Thunderstorm with light drizzle", "Thunderstorm with drizzle",
-                        "Thunderstorm with heavy drizzle", "Thunderstorm with Hail",
-                        "Light shower sleet", "Shower sleet", "Light rain and snow", "Rain and snow",
-                        "Light shower snow", "Shower snow", "Heavy shower snow"],
-    "Snow Conditions": ["Light snow showers", "Moderate snow showers", "Heavy snow showers",
-                        "Light snow", "Moderate snow", "Heavy snow", "Sleet", "Shower sleet",
-                        "Light shower snow", "Shower snow", "Heavy shower snow"]
-}
+#     weather_descriptions = {
+#     "Clear Conditions": ["Clear sky"],
+#     "Cloudy Conditions": ["Few clouds", "Scattered clouds", "Broken clouds", "Overcast clouds"],
+#     "Atmospheric Conditions": ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand, dust whirls", 
+#                                "Dust, low visibility", "Sand"],
+#     "Poor Visibility Conditions": ["Haze", "Dust, low visibility"],
+#     "Windy Conditions": ["Squalls", "Tornado"],
+#     "Rain Conditions": ["Light rain showers", "Moderate rain showers", "Heavy rain showers",
+#                         "Light rain", "Moderate rain", "Heavy rain", "Thunderstorm with light rain",
+#                         "Thunderstorm with moderate rain", "Thunderstorm with heavy rain",
+#                         "Thunderstorm with light drizzle", "Thunderstorm with drizzle",
+#                         "Thunderstorm with heavy drizzle", "Thunderstorm with Hail",
+#                         "Light shower sleet", "Shower sleet", "Light rain and snow", "Rain and snow",
+#                         "Light shower snow", "Shower snow", "Heavy shower snow"],
+#     "Snow Conditions": ["Light snow showers", "Moderate snow showers", "Heavy snow showers",
+#                         "Light snow", "Moderate snow", "Heavy snow", "Sleet", "Shower sleet",
+#                         "Light shower snow", "Shower snow", "Heavy shower snow"]
+# }
 
 
-    if response.status_code == 200:
-        weather = data['weather'][0]['description']
-        speak(f'It is {weather} outside')
+#     if response.status_code == 200:
+#         weather = data['weather'][0]['description']
+#         speak(f'It is {weather} outside')
     
-    else:
-        speak("Couldn't get the weather information sir")
+#     else:
+#         speak("Couldn't get the weather information sir")
 
 if __name__ == "__main__":
     wish()
-    #takecommand()
+    takecommand()
     while True:
         query = takecommand().lower()
         sites = [['youtube', 'https://www.youtube.com/'], ['google', 'https://www.youtube.com/]']]
         for site in sites:
-        if f"open {}" in query:
-            webbrowser.open("https://www.youtube.com/")
-            speak('Opening YouTube')
+            if f"open {'youtube'}" in query:
+                webbrowser.open("https://www.youtube.com/")
+                speak('Opening YouTube')
         
 
         #logic buildind or task
